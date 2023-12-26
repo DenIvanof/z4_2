9a974f13f1450a27fe892634e8dbe12478d6736c
Ivanich <deniska090798@gmail.com>
2023-12-26 22:26:44 +0400

    new

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,20 @@ public class Main {
         BmiService service = new BmiService();
         double height;
         int mass;
-        height=1.87;
-        mass=98;
+        height = 1.87;
+        mass = 98;
         System.out.println();
-        System.out.println("Расчёт BMI-индекса при росте "+(height) + "м и весе "+ (mass)+"кг.");
-        System.out.println("BMI-индекс:"+ service.calculate(height,mass));
+        System.out.println("Расчёт BMI-индекса при росте " + (height) + "м и весе " + (mass) + "кг.");
+        System.out.println("BMI-индекс:" + service.calculate(height, mass));
 
         System.out.println();
         System.out.println("Тест");
-        System.out.println(service.calculate(1.87,98));
-        System.out.println(service.calculate(1.50,100));
-        System.out.println(service.calculate(1.60,50));
-        System.out.println(service.calculate(1.87,107));
-        System.out.println(service.calculate(1.87,90));
-        System.out.println(service.calculate(2,90));
+        System.out.println(service.calculate(1.87, 98));
+        System.out.println(service.calculate(1.50, 100));
+        System.out.println(service.calculate(1.60, 50));
+        System.out.println(service.calculate(1.87, 107));
+        System.out.println(service.calculate(1.87, 90));
+        System.out.println(service.calculate(2, 90));
 
 
     }

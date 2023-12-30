58b031b4a3583c177c466426a7a173c0017f5f62
Ivanich <deniska090798@gmail.com>
2023-12-30 09:54:12 +0400

    new2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public int calculate (double height, int mass) {
-        int n=2;
+    public int calculate(double height, int mass) {
+        int n = 2;
         int index;
         index = (int) (mass / (Math.pow(height, n)));
-        return  index;
+        return index;
     }
 }
